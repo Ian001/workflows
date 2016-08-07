357ba214a3bc1b9968b74a115cba448477d8de65
ian <ian_scott40@hotmail.com>
2016-08-07 18:33:08 +0800

    added static file monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,8 @@ var jsSources = [
   'components/scripts/template.js'
 ];
 var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 
 gulp.task('coffee', function(){
    gulp.src(coffeeSources)
@@ -48,6 +50,8 @@ gulp.task('watch', function(){
   gulp.watch(coffeeSources, ['coffee']);
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
+  gulp.watch(htmlSources, ['html']);
+  gulp.watch(jsonSources, ['json']);
 
 });
 
@@ -58,5 +62,15 @@ gulp.task('connect', function(){
    });
 });
 
+gulp.task('html', function(){
+   gulp.src(htmlSources)
+   .pipe(connect.reload())   
 
-gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
+});
+gulp.task('json', function(){
+   gulp.src(jsonSources)
+   .pipe(connect.reload())   
+
+});
+
+gulp.task('default', ['html', 'coffee', 'js', 'compass', 'connect', 'watch']);
