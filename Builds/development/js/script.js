b993ef40a3c7298934033eddfce6c666645452ce
ian <ian_scott40@hotmail.com>
2016-08-07 18:00:40 +0800

    added live reload

--- Builds/development/js/script.js
+++ Builds/development/js/script.js
@@ -77,7 +77,7 @@ $ = require('jquery');
 
 (fill = function(item) {
   return $('.tagline').append("" + item);
-})('The most creative minds in Art Ian is best');
+})('The most creative minds in Art');
 
 fill;
 
--- components/scripts/tagline.js
+++ components/scripts/tagline.js
@@ -4,6 +4,6 @@ $ = require('jquery');
 
 (fill = function(item) {
   return $('.tagline').append("" + item);
-})('The most creative minds in Art Ian is best');
+})('The most creative minds in Art');
 
 fill;
